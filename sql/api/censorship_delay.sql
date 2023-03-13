a842c68b1f033cb7f25f1c9f6007e33e93e09a54
OreoMev <122867899+OreoMev@users.noreply.github.com>
2023-03-13 12:58:58 +0100

    blocksdelay > 0

--- sql/api/censorship_delay.sql
+++ sql/api/censorship_delay.sql
@@ -1,8 +1,8 @@
 SELECT
-  COUNT(*) FILTER (WHERE blocksdelay > 1 and transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0 ) AS censored_tx_count,
-  AVG(delay) FILTER (WHERE blocksdelay > 1 and transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0)::float AS censored_avg_delay,
-  COUNT(*) FILTER (WHERE blocksdelay <= 1) AS uncensored_tx_count,
-  AVG(delay) FILTER (WHERE blocksdelay <= 1)::float AS uncensored_avg_delay
+  COUNT(*) FILTER (WHERE blocksdelay > 0 and transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0 ) AS censored_tx_count,
+  AVG(delay) FILTER (WHERE blocksdelay > 0 and transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0)::float AS censored_avg_delay,
+  COUNT(*) FILTER (WHERE blocksdelay = 0) AS uncensored_tx_count,
+  AVG(delay) FILTER (WHERE blocksdelay = 0)::float AS uncensored_avg_delay
 FROM
   transactions_data
 WHERE
