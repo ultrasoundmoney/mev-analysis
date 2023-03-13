fe05c4f396a59ea462291e0aaa37f54e6c2fa38e
OreoMev <122867899+OreoMev@users.noreply.github.com>
2023-03-13 11:48:36 +0100

    Update this API to fix methodology

--- sql/api/censored_txs.sql
+++ sql/api/censored_txs.sql
@@ -12,6 +12,6 @@ SELECT
      transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested
    )
    = 0
-   AND transactions_data.blocksdelay > 0
    AND transactions_data.blacklist != '{NULL}'
+   AND transactions_data.blocksdelay > 1
    AND transactions_data.mined > (CURRENT_DATE - $1::interval)
