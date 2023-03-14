2c36ce2b5f77a5a5ef13252840ec3872c7226962
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-03-14 11:57:46 +0100

    fix: delay queries

--- sql/api/censorship_delay.sql
+++ sql/api/censorship_delay.sql
@@ -1,6 +1,12 @@
 SELECT
-  COUNT(*) FILTER (WHERE blocksdelay > 0 ) AS censored_tx_count,
-  AVG(delay) FILTER (WHERE blocksdelay > 0)::float AS censored_avg_delay,
+  COUNT(*) FILTER (WHERE blocksdelay > 0
+                   AND transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0)
+    AS censored_tx_count,
+
+  AVG(delay) FILTER (WHERE blocksdelay > 0
+                     AND transactions_data.lowbasefee + transactions_data.lowtip + transactions_data.congested = 0)::float
+    AS censored_avg_delay,
+
   COUNT(*) FILTER (WHERE blocksdelay = 0) AS uncensored_tx_count,
   AVG(delay) FILTER (WHERE blocksdelay = 0)::float AS uncensored_avg_delay
 FROM
