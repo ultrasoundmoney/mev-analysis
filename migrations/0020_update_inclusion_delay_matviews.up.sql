d3afe87ac2d65c9f9c08de73fbc5100a892ae229
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-03-28 13:07:24 +0200

    feat(censorship): update inclusion delay matviews

---
+++ migrations/0020_update_inclusion_delay_matviews.up.sql
@@ -0,0 +1,266 @@
+DROP MATERIALIZED VIEW IF EXISTS inclusion_delay_7d;
+DROP MATERIALIZED VIEW IF EXISTS inclusion_delay_30d;
+
+CREATE MATERIALIZED VIEW inclusion_delay_7d AS
+SELECT
+	0 AS avg_delay,
+	0 AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'miner'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 1
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'optimal'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay = 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'low_base_fee'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 1
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay > 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'low_tip'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 1
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay > 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'likely_insufficient_balance'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.low_balance = 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'congested'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.congested = 1
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay > 0
+	AND transactions_data.low_balance != 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'borderline'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay = 1
+  AND transactions_data.blacklist = '{NULL}'::text[]
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'ofac_delayed'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay > 0
+	AND transactions_data.blacklist != '{NULL}'::text[]
+	AND transactions_data.low_balance != 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'unknown'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '7 days'::interval)
+	AND transactions_data.blocksdelay > 1
+	AND transactions_data.low_balance != 1
+	AND transactions_data.blacklist = '{NULL}'::text[]
+WITH NO DATA;
+
+CREATE MATERIALIZED VIEW inclusion_delay_30d AS
+SELECT
+	0 AS avg_delay,
+	0 AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'miner'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 1
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'optimal'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay = 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'low_base_fee'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 1
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay > 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'low_tip'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 1
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay > 0
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'likely_insufficient_balance'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.low_balance = 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'congested'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.congested = 1
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay > 0
+	AND transactions_data.low_balance != 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'borderline'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay = 1
+  AND transactions_data.blacklist = '{NULL}'::text[]
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'ofac_delayed'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay > 0
+	AND transactions_data.blacklist != '{NULL}'::text[]
+	AND transactions_data.low_balance != 1
+UNION
+SELECT
+	AVG(transactions_data.delay) AS avg_delay,
+	AVG(transactions_data.blocksdelay) AS avg_block_delay,
+	COUNT(transactions_data.transaction_hash) AS n,
+	'unknown'::text AS t_type
+FROM
+	transactions_data
+WHERE
+	transactions_data.minertransaction = 0
+	AND transactions_data.congested = 0
+	AND transactions_data.lowbasefee = 0
+	AND transactions_data.lowtip = 0
+	AND transactions_data.mined > (CURRENT_DATE - '30 days'::interval)
+	AND transactions_data.blocksdelay > 1
+	AND transactions_data.low_balance != 1
+	AND transactions_data.blacklist = '{NULL}'::text[]
+WITH NO DATA;
