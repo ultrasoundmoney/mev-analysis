d4c781de8b8a336869b788121ab38bed878b145f
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-06-06 13:21:15 +0200

    feat: actually promote eligible demoted builders

--- src/phoenix/promotion_monitor.rs
+++ src/phoenix/promotion_monitor.rs
@@ -1,13 +1,16 @@
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use itertools::Itertools;
-use sqlx::PgPool;
-use tracing::{info, warn};
+use sqlx::{PgPool, Row};
+use tracing::{debug, info, warn};
+
+use crate::env::ToNetwork;
 
 use super::{
     alert,
     checkpoint::{self, CheckpointId},
     demotion_monitor::{get_builder_demotions, BuilderDemotion},
+    env::APP_CONFIG,
 };
 
 async fn get_missed_slots(mev_pool: &PgPool, start: &DateTime<Utc>) -> Result<Vec<i64>> {
@@ -24,51 +27,92 @@ async fn get_missed_slots(mev_pool: &PgPool, start: &DateTime<Utc>) -> Result<Ve
     .map_err(Into::into)
 }
 
-#[derive(Debug, PartialEq)]
-struct BuilderPromotion {
-    builder_pubkey: String,
-    builder_id: String,
+async fn promote_builder_ids(
+    relay_pool: &PgPool,
+    builder_ids: &Vec<String>,
+) -> Result<Vec<(String, String)>> {
+    let query = format!(
+        "
+        UPDATE {}_blockbuilder
+        SET is_optimistic = true
+        WHERE
+            builder_id = ANY($1)
+        AND collateral > 0
+        AND is_optimistic = false
+        RETURNING builder_id, builder_pubkey
+        ",
+        APP_CONFIG.env.to_network().to_string()
+    );
+
+    sqlx::query(&query)
+        .bind(builder_ids)
+        .fetch_all(relay_pool)
+        .await
+        .map(|rows| {
+            rows.iter()
+                .map(|row| (row.get("builder_id"), row.get("builder_pubkey")))
+                .collect_vec()
+        })
+        .map_err(Into::into)
 }
 
-fn get_eligible_builders(
-    demotions: Vec<BuilderDemotion>,
-    missed_slots: Vec<i64>,
-) -> Vec<BuilderPromotion> {
-    info!(
+fn format_builder_list(builders: &Vec<(String, String)>) -> String {
+    builders
+        .iter()
+        .sorted_by_key(|(id, _)| id)
+        .group_by(|(id, _)| id.clone())
+        .into_iter()
+        .fold(String::new(), |acc, (id, group)| {
+            let pubkeys = group
+                .map(|(_, pubkey)| format!("`{}`", &pubkey))
+                .collect_vec()
+                .join("\n");
+
+            if acc.is_empty() {
+                format!("*{}*\n{}", id, pubkeys)
+            } else {
+                format!("{}\n\n*{}*\n{}", acc, id, pubkeys)
+            }
+        })
+}
+
+const ELIGIBLE_ERRORS: &[&str] = &[
+    "json error: request timeout hit before processing",
+    "simulation failed: unknown ancestor",
+    "Post \"http://prio-load-balancer:80\": context deadline exceeded (Client.Timeout exceeded while awaiting headers)"
+];
+
+fn get_eligible_builders(demotions: Vec<BuilderDemotion>, missed_slots: Vec<i64>) -> Vec<String> {
+    debug!(
         "get_eligible_builders: demotions: {:?}, missed_slots {:?}",
         &demotions, &missed_slots
     );
-    let eligible_errors = vec![
-        "json error: request timeout hit before processing",
-        "simulation failed: unknown ancestor",
-    ];
     demotions
         .into_iter()
-        .sorted_by_key(|d| d.builder_pubkey.clone())
-        .group_by(|d| d.builder_pubkey.clone())
+        .sorted_by_key(|d| d.builder_id.clone())
+        .group_by(|d| d.builder_id.clone())
         .into_iter()
-        .filter_map(|(pubkey, group)| {
-            let demotions = group.collect_vec();
-            info!("grouped demotions for {}: {:?}", &pubkey, &demotions);
-            let no_missed_slots = demotions.iter().all(|d| !missed_slots.contains(&d.slot));
-            let only_eligible_errors = demotions
-                .iter()
-                .all(|d| eligible_errors.contains(&d.sim_error.as_str()));
-            let builder_id = demotions[0].builder_id.clone();
+        .filter_map(|(builder_id, group)| match builder_id {
+            None => {
+                warn!(
+                    "{} pubkeys without builder_id, unable to promote",
+                    &group.count()
+                );
+                None
+            }
+            Some(builder_id) => {
+                let demotions = group.collect_vec();
+                debug!("grouped demotions for {}: {:?}", &builder_id, &demotions);
+                let no_missed_slots = demotions.iter().all(|d| !missed_slots.contains(&d.slot));
+                let only_eligible_errors = demotions
+                    .iter()
+                    .all(|d| ELIGIBLE_ERRORS.contains(&d.sim_error.as_str()));
 
-            match (no_missed_slots, only_eligible_errors, builder_id) {
-                (true, true, Some(builder_id)) => Some(BuilderPromotion {
-                    builder_pubkey: pubkey,
-                    builder_id,
-                }),
-                (_, _, None) => {
-                    warn!(
-                        "no builder_id found for pubkey {}, unable to promote",
-                        &pubkey
-                    );
+                if no_missed_slots && only_eligible_errors {
+                    Some(builder_id)
+                } else {
                     None
                 }
-                _ => None,
             }
         })
         .collect()
@@ -99,15 +143,12 @@ pub async fn run_promotion_monitor(
     let eligible = get_eligible_builders(demotions, missed_slots);
 
     if !eligible.is_empty() {
-        info!("found {} builders eligible for promotion", &eligible.len());
+        info!("found builder ids eligible for promotion: {:?}", &eligible);
 
-        let builder_list = eligible
-            .iter()
-            .map(|b| format!("*{}* `{}`", b.builder_id, b.builder_pubkey))
-            .collect_vec()
-            .join("\n");
+        let promoted = promote_builder_ids(&relay_pool, &eligible).await?;
+        let builder_list = format_builder_list(&promoted);
 
-        let message = format!("promoted the following builders:\n\n{}", builder_list);
+        let message = format!("*promoted the following builders*:\n\n{}", builder_list);
 
         alert::send_telegram_alert(&message).await?;
     }
@@ -142,19 +183,7 @@ mod tests {
         let missed_slots = vec![];
 
         let result = get_eligible_builders(demotions, missed_slots);
-        assert_eq!(
-            result,
-            vec![
-                BuilderPromotion {
-                    builder_pubkey: "pubkey1".to_string(),
-                    builder_id: "builder1".to_string(),
-                },
-                BuilderPromotion {
-                    builder_pubkey: "pubkey2".to_string(),
-                    builder_id: "builder2".to_string(),
-                }
-            ]
-        );
+        assert_eq!(result, vec!["builder1".to_string(), "builder2".to_string()]);
     }
 
     #[test]
@@ -180,7 +209,7 @@ mod tests {
 
         let result = get_eligible_builders(demotions, missed_slots);
 
-        assert_eq!(result, vec![]);
+        assert_eq!(result, Vec::<String>::new());
     }
 
     #[test]
@@ -213,13 +242,7 @@ mod tests {
 
         let result = get_eligible_builders(demotions, missed_slots);
 
-        assert_eq!(
-            result,
-            vec![BuilderPromotion {
-                builder_pubkey: "pubkey1".to_string(),
-                builder_id: "builder1".to_string(),
-            }]
-        );
+        assert_eq!(result, vec!["builder1".to_string()]);
     }
 
     #[test]
@@ -252,12 +275,20 @@ mod tests {
 
         let result = get_eligible_builders(demotions, missed_slots);
 
+        assert_eq!(result, vec!["builder1".to_string()]);
+    }
+
+    #[test]
+    fn test_format_builder_list() {
+        let builder_ids = vec![
+            ("builder1".to_string(), "pkey1".to_string()),
+            ("builder2".to_string(), "pkey2".to_string()),
+            ("builder1".to_string(), "pkey3".to_string()),
+        ];
+        let result = format_builder_list(&builder_ids);
         assert_eq!(
             result,
-            vec![BuilderPromotion {
-                builder_pubkey: "pubkey1".to_string(),
-                builder_id: "builder1".to_string(),
-            }]
+            "*builder1*\n`pkey1`\n`pkey3`\n\n*builder2*\n`pkey2`"
         );
     }
 }
