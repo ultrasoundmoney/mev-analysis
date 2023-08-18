92826f143ffed0a9ec57aee7ff812bd5a4289532
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-08-18 10:17:13 +0200

    chore: remove unused code

--- src/serve/relay_redis.rs
+++ src/serve/relay_redis.rs
@@ -1,7 +1,3 @@
-use std::collections::HashSet;
-
-use chrono::{Duration, Utc};
-
 use crate::env::ToNetwork;
 
 use super::APP_CONFIG;
@@ -18,35 +14,4 @@ pub async fn get_known_validator_count(client: &redis::Client) -> anyhow::Result
         .query_async(&mut conn)
         .await
         .map_err(Into::into)
-}
-
-pub async fn get_active_validator_count(client: &redis::Client) -> anyhow::Result<i64> {
-    let active_validators_hours = 3; // default in mev-boost-relay
-    let now = Utc::now();
-    let keys = (0..active_validators_hours)
-        .map(|i| {
-            let hour = now.checked_sub_signed(Duration::hours(i)).unwrap();
-            format!(
-                "boost-relay/{}:active-validators:{}",
-                &APP_CONFIG.env.to_network().to_string(),
-                hour.format("%Y-%m-%dT%H").to_string()
-            )
-        })
-        .collect::<Vec<String>>();
-
-    let mut validators = HashSet::<String>::new();
-    let mut conn = client.get_async_connection().await?;
-
-    for key in keys {
-        let entries = redis::cmd("HGETALL")
-            .arg(key)
-            .query_async::<_, Vec<(String, String)>>(&mut conn)
-            .await?
-            .into_iter()
-            .map(|(k, _)| k);
-
-        validators.extend(entries);
-    }
-
-    Ok(validators.len() as i64)
 }
