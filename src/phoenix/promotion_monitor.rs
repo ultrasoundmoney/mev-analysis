34be0233e729e23667bb670b4432aee7d690d717
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-12-25 10:31:05 +0200

    fix(promotion_monitor): match against full error

--- src/phoenix/promotion_monitor.rs
+++ src/phoenix/promotion_monitor.rs
@@ -77,7 +77,7 @@ fn format_builder_list(builders: &Vec<(String, String)>) -> String {
 }
 
 pub const ELIGIBLE_ERRORS: &[&str] = &[
-    "HTTP status server error (500 Internal Server Error) for url",
+    "HTTP status server error (500 Internal Server Error) for url (http://prio-load-balancer/)",
     "Post \"http://prio-load-balancer:80\": context deadline exceeded (Client.Timeout exceeded while awaiting headers)",
     "json error: request timeout hit before processing",
     "simulation failed: unknown ancestor",
